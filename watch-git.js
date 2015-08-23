bff8008a09b4760e808f532836aa4b8f0f78fc11
dev <parksungho86@gmail.com>
2015-08-23 12:19:24 +0900

    init

--- watch-git.js
+++ watch-git.js
@@ -26,13 +26,16 @@ handler.on('error', function (err) {
 });
 
 handler.on('push', function (event) {
-    logger.info(exec('cp nginx.conf /etc/nginx/nginx.conf').stdout);
-    logger.info(exec('service nginx reload').stdout);
 
     logger.info('Received a push event for %s to %s',
         event.payload.repository.name,
         event.payload.ref);
     logger.info(exec('git pull').stdout);
+
+    logger.info(exec('cp nginx.conf /etc/nginx/nginx.conf').stdout);
+    logger.info(exec('service nginx reload').stdout);
+
+
     logger.info(exec('npm install').stdout);
     logger.info(exec('grunt').stdout);
     serverRestart();
