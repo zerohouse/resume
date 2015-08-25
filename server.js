bad973cebe14f163204febfad76cdcab8f6aac9b
dev <parksungho86@gmail.com>
2015-08-25 12:22:47 +0900

    init

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 //app.use('/', express.static('dist'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/style.css'));
 });
 
 
