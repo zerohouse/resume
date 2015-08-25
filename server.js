b6df16612c0db76d171411e7025d5f5e6d61ff40
dev <parksungho86@gmail.com>
2015-08-25 12:22:17 +0900

    init

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('./server/app.js');
 var http = require('http').Server(app);
 var path = require('path');
 
-app.use('/', express.static('dist'));
+//app.use('/', express.static('dist'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
