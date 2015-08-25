f2ed5ebe77c3d4e255ef8107e5fbb653863d19f3
dev <parksungho86@gmail.com>
2015-08-25 12:19:48 +0900

    init

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var app = require('./server/app.js');
 var http = require('http').Server(app);
 
+
+app.use('/', express.static('dist'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
--- server/app.js
+++ server/app.js
@@ -12,10 +12,4 @@ require('./upload.js')(app);
 require('./session.js')(app);
 require('./route.js')(app, logger);
 
-app.use('/', express.static('dist'));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
 module.exports = app;
