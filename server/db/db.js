621dc62f51d3e4eba25e0bff46fec729dae14764
park <parksungho86@gmail.com>
2015-08-27 16:29:49 +0900

    init

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,8 @@
-var db = {};
+var db = {},
+    mongoose = require('mongoose'),
+    ObjectId = mongoose.Types.ObjectId();
+mongoose.connect('mongodb://localhost:27017/resume');
 db.abc = 'ab';
+
+
 module.exports = db;
