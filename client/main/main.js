e986029ae8731d8747cdd771f64a79651c6c4bd6
dev <parksungho86@gmail.com>
2015-09-05 21:48:06 +0900

    timeline

--- client/main/main.js
+++ client/main/main.js
@@ -1,3 +1,5 @@
 app.controller('main', function ($scope) {
 
+    $scope.skills = ['JAVA', 'Javascript', 'AngularJS', 'NodeJS', 'Spring MVC', 'Java WebServlet', 'MongoDB', 'Mysql', 'Redis', 'Less', 'Css3', 'Html5', 'SocketIO', 'NginX'];
+
 });
